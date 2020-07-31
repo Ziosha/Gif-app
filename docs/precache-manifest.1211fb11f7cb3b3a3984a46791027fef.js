9bdd6c24aae65139d9f218979f26b3477b986ae9
Gustavo Bravo <zioshadarkmetal@gmail.com>
2020-07-31 19:56:50 -0400

    docs agregado

---
+++ docs/precache-manifest.1211fb11f7cb3b3a3984a46791027fef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86e16139541056dc9e2c0e14943bf73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c273e98a5a7f7f3f7a7f",
+    "url": "/static/css/main.9cf29f7f.chunk.css"
+  },
+  {
+    "revision": "ca214e944c81a5f0546a",
+    "url": "/static/js/2.63bebacb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.63bebacb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c273e98a5a7f7f3f7a7f",
+    "url": "/static/js/main.66742817.chunk.js"
+  },
+  {
+    "revision": "07298e96349337c2e944",
+    "url": "/static/js/runtime-main.b3354861.js"
+  }
+]);
